5afe445babad86304aa78349ec7b8c0ffa6133ea
unrar <catbuntu@catbuntu.me>
2013-08-07 18:10:02 +0200

    Initial damn commit
    Yes it's funny isn't it?

---
+++ Languator/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace Languator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(173, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cześć!";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(114, 86);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(218, 177);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "English:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 340);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Język Polski:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(118, 289);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(213, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(118, 337);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(214, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(360, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(81, 65);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Translate!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 458);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Languator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+
+    }
+}
+
---
+++ Languator/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+
+namespace Languator
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Dictionary with the words in (English, Polski)
+        /// </summary>
+        private Dictionary<string, string> trans = new Dictionary<string, string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            // Start the dicts
+            InitializeDictionary();
+        }
+
+        /// <summary>
+        /// Initialize the dictionaries. Add here new translations.
+        /// </summary>
+        private void InitializeDictionary()
+        {
+            // ENGLISH to POLSKI
+            trans.Add("hello", "cześć");
+            trans.Add("bye", "cześć");
+            trans.Add("goodbye", "da widzanie");
+        }
+   
+        private void translatePolskiEnglish(String text)
+        {
+            if (trans.ContainsValue(text))
+            {
+                List<string> matches = new List<string>();
+                foreach (var pair in trans)
+                {
+                    if (pair.Value == text)
+                    {
+                        matches.Add(CultureInfo.CurrentCulture.TextInfo.ToTitleCase(pair.Key));
+                    }
+                }
+                String allthematches = String.Join(", ", matches.ToArray());
+                textBox1.Text = allthematches;
+            }
+            else
+            {
+                MessageBox.Show("Translation not found", "Error", MessageBoxButtons.OK ,MessageBoxIcon.Error);
+            }
+        }
+        private void translateEnglishPolski(String text)
+        {
+            if (trans.ContainsKey(text))
+            {
+                textBox2.Text = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(trans[text]);
+            }
+            else
+            {
+                MessageBox.Show("Translation not found", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This program has been develop by the Zayn Malik lovers team in Poland.",
+                            "Vas happenin?", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                MessageBox.Show("Please, void one of both textboxes.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else if (textBox1.Text == "" && textBox2.Text != "")
+            {
+                translatePolskiEnglish(textBox2.Text.ToLower());
+            }
+            else if (textBox2.Text == "" && textBox1.Text != "")
+            {
+                translateEnglishPolski(textBox1.Text.ToLower());
+            }
+            else
+            {
+                MessageBox.Show("You must write something in either one of both textboxes!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+
+    }
+}
